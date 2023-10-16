4279c29a2721bd4ca5b21c68134e0f3d2951d1d6
Diego <willslipoknotpsico@gmail.com>
2023-10-16 13:27:40 -0500

    aa

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:'containers-us-west-101.railway.app:5877/',
+    baseURL:'http://localhost:5877',
     withCredentials:true,
 
 })
