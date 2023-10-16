c7d74143277792eaccedd1ef143a818e2794bf27
Diego <willslipoknotpsico@gmail.com>
2023-10-16 15:06:44 -0500

    gol

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'https://containers-us-west-88.railway.app:5586',
-    withCredentials:true,
+  baseURL: 'https://containers-us-west-88.railway.app:5586',
+  withCredentials: true,
+});
 
-})
-
-export default instance
+export default instance;
