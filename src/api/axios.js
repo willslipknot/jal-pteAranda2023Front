dd63072748c10133583beec826d18f4a76fc38fa
Diego <willslipoknotpsico@gmail.com>
2023-10-16 09:05:42 -0500

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:'http://localhost:3001',
+    baseURL:'https://jac-ptearanda2023.onrender.com',
     withCredentials:true,
 
 })
