052c9e5089520972d5818208af6384dde0b0ca3d
Diego <willslipoknotpsico@gmail.com>
2023-10-16 12:45:04 -0500

    sa

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:'postgres://postgres:djWhRM6xB8LtrnwRWsbE@containers-us-west-101.railway.app:5877/railway/',
+    baseURL:'containers-us-west-101.railway.app/',
     withCredentials:true,
 
 })
