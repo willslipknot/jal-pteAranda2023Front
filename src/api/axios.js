76549e63c13844ac8515bedba2a7896236b2e0c0
Diego <willslipoknotpsico@gmail.com>
2023-10-16 14:58:06 -0500

    asad

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:'https://containers-us-west-88.railway.app',
+    baseURL:'https://containers-us-west-88.railway.app:5586',
     withCredentials:true,
 
 })
