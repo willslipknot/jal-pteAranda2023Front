c0a8bdb46ecc10f5ac046b2e28d76c8672fb9f39
wills <willslipknotpsico@gmail.com>
2023-10-18 10:06:16 -0500

    aver

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'; 
 import '../assets/css/footer.css'; 
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
     render() {
         return (
 
   <footer className="footer">
-    <p>Chronus King Group   ©Derechos Reservados</p><p></p>
+    <Link to="/loginAdmin"><p>Chronus</p></Link><p>King Group   ©Derechos Reservados</p><p></p>
   </footer>
 );
   
