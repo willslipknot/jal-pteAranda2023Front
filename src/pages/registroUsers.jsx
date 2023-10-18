e4476e6fd2105193efee501e9215db4087a012e7
wills <willslipknotpsico@gmail.com>
2023-10-18 09:28:37 -0500

    d

--- src/pages/registroUsers.jsx
+++ src/pages/registroUsers.jsx
@@ -37,7 +37,6 @@ function RegistroUsers() {
       }
     };
 
-    // Llama a la función para obtener la IP pública al cargar el componente
     getPublicIP();
   }, []);
 
@@ -74,7 +73,7 @@ function RegistroUsers() {
             <input type="text" {...register('correo', { required: true })} />
             {errors.correo && <p className='mensajes'>Correo es requerido</p>}
             <input type='text' value={"Votante"} hidden {...register('tipo', { required: true })} />
-            <input type='text' value={ip} hidden  {...register('ip', { required: true })} />
+            <input type='text' value={ip}  {...register('ip', { required: true })} />
           </div>
           <div className="form-group-Home">
             <button type="submit">Votar</button>
