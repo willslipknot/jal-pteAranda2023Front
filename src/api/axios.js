f73b7cc709897aef98457947572ac0b78d378c52
Diego <willslipoknotpsico@gmail.com>
2023-10-18 15:53:38 -0500

    ss

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5586',
-  //baseURL: 'https://jal-ptearanda2023back-production.up.railway.app',
+  //baseURL: 'http://localhost:5586',
+  baseURL: 'https://jal-ptearanda2023back-production.up.railway.app',
   withCredentials: true,
 });
 
