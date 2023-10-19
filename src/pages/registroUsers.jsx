6e4876b9e7adb0abe99fd447d4bce44a3ea381fc
willslipknot <62850730+willslipknot@users.noreply.github.com>
2023-10-19 12:32:53 -0500

    Update registroUsers.jsx

--- src/pages/registroUsers.jsx
+++ src/pages/registroUsers.jsx
@@ -9,91 +9,91 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 function RegistroUsers() {
-  const { register, handleSubmit, reset, formState: { errors } } = useForm();
-  const { signup, isAuthenticated, errors: userErrors } = useUser();
-  const navigate = useNavigate();
-  const [localIP, setLocalIP] = useState("");
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const { signup, isAuthenticated, errors: userErrors } = useUser();
+    const navigate = useNavigate();
+    const [localIP, setLocalIP] = useState("");
 
-  useEffect(() => {
-    if (isAuthenticated) navigate('/Votar');
+    useEffect(() => {
+        if (isAuthenticated) navigate('/Votar');
 
-  }, [isAuthenticated,]);
+    }, [isAuthenticated,]);
 
-  const validateEmail = (email) => {
+    const validateEmail = (email) => {
 
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-  };
-
-  useEffect(() => {
-    const getLocalIP = async () => {
-      try {
-        const response = await axios.get('https://api64.ipify.org?format=json'); 
-        setLocalIP(response.data.ip);
-      } catch (error) {
-        console.error('Error al obtener la IP local:', error);
-      }
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
     };
 
-    getLocalIP();
-  }, []);
+    useEffect(() => {
+        const getLocalIP = async () => {
+            try {
+                const response = await axios.get('https://api64.ipify.org?format=json');
+                setLocalIP(response.data.ip);
+            } catch (error) {
+                console.error('Error al obtener la IP local:', error);
+            }
+        };
 
-  const onSubmit = handleSubmit(async (data) => {
-    console.log('Datos del formulario:', data);
-    if (!validateEmail(data.correo)) {
-      console.log('Correo electrónico inválido');
-      alert('Correo electrónico inválido');
-      return;
-    } else {
-      data.ip = localIP;
-      await signup(data);
+        getLocalIP();
+    }, []);
 
-      reset();
-    }
+    const onSubmit = handleSubmit(async (data) => {
+        console.log('Datos del formulario:', data);
+        if (!validateEmail(data.correo)) {
+            console.log('Correo electrónico inválido');
+            alert('Correo electrónico inválido');
+            return;
+        } else {
+            data.ip = localIP;
+            await signup(data);
 
-  });
+            reset();
+        }
 
-  return (
-    <div className='todo'>
-      <br></br>
-      <h1 className='nombre1'>Intencion de voto Puente Aranda Bogotá 2023</h1>
-      <div className="form-container-Home">
-        <div className="encabezado-container">
-          <img className='imagen_bogota' src={bogota} alt=" " />
-          <img className='imagen_colombia' src={colombia} alt=" " />
-        </div>
-        <form className="formulario-Home" onSubmit={onSubmit}>
-          <div className="form-group-Home">
-            <label htmlFor="nombre">Nombres</label>
-            <input type="text" {...register('nombre', { required: true })} />
-            {errors.nombre && <p className='mensajes'>Nombre es requerido</p>}
-          </div>
-          <div className="form-group-Home">
-            <label htmlFor="correo">Correo</label>
-            <input type="text" {...register('correo', { required: true })} />
-            {errors.correo && <p className='mensajes'>Correo es requerido</p>}
-            <input type='text' value={"Votante"} hidden {...register('tipo', { required: true })} />
-            <input type='text' value={localIP} hidden readOnly/>
-          </div>
-          <div className="form-group-Home">
-            <button type="submit">Votar</button>
+    });
+
+    return (
+        <div className='todo'>
             <br></br>
-            <br></br>
-            <Link to="/Resultados"> <button type="button">Resultados</button> </Link>
-          </div>
-          {userErrors.map((error, i) => (
-            <div className='mensajes' key={i}>
-              {error}
+            <h1 className='nombre1'>Intencion de voto Puente Aranda Bogotá 2023</h1>
+            <div className="form-container-Home">
+                <div className="encabezado-container">
+                    <img className='imagen_bogota' src={bogota} alt=" " />
+                    <img className='imagen_colombia' src={colombia} alt=" " />
+                </div>
+                <form className="formulario-Home" onSubmit={onSubmit}>
+                    <div className="form-group-Home">
+                        <label htmlFor="nombre">Nombres</label>
+                        <input type="text" {...register('nombre', { required: true })} />
+                        {errors.nombre && <p className='mensajes'>Nombre es requerido</p>}
+                    </div>
+                    <div className="form-group-Home">
+                        <label htmlFor="correo">Correo</label>
+                        <input type="text" {...register('correo', { required: true })} />
+                        {errors.correo && <p className='mensajes'>Correo es requerido</p>}
+                        <input type='text' value={"Votante"} hidden {...register('tipo', { required: true })} />
+                        <input type='text' value={localIP} hidden readOnly />
+                    </div>
+                    <div className="form-group-Home">
+                        <button type="submit">Votar</button>
+                        <br></br>
+                        <br></br>
+                        <Link to="/Resultados"> <button type="button">Resultados</button> </Link>
+                    </div>
+                    {userErrors.map((error, i) => (
+                        <div className='mensajes' key={i}>
+                            {error}
+                        </div>
+                    ))}
+                    {userErrors.forEach((error, i) => {
+                        alert(error);
+                    })}
+
+                </form>
             </div>
-          ))}
-
-          userErrors.forEach((error, i) => {
-          alert(error); 
-          });
-        </form>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default RegistroUsers;
